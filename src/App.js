f62394ac8889f86ff23e3aab9a83c0c3cb6b462f
Sasha <isasha96@gmail.com>
2019-04-26 19:11:53 +0300

    current screen state added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,44 @@ import './App.css';
 import Message from './Message';
 import Navigation from "./Navigation";
 class App extends Component {
+
+  screens = {
+    GENERAL: 'general',
+    EXAMS: 'exams',
+    STUDY_PLAN: 'study_plan',
+    SCHEDULE: 'schedule'
+  };
+
+  constructor(){
+    super();
+    this.state = {
+      currentScreen: this.screens.GENERAL
+    };
+  }
+
+  _setScreen(button){
+    // todo
+    console.log(button);
+  }
+
   render() {
+    let currentScreenComponent = null;
+    if(this.state.currentScreen = this.screens.GENERAL){
+      currentScreenComponent = <Message headline="ביטול שיעור" content="השיעור באלגברה לינארית בתאריך 10.4.2019 מבוטל"/>;
+    }
+    else if (this.state.currentScreen = this.screens.STUDY_PLAN){
+      // set the currentScreenComponent to studyPlan component
+    }
+    else if (this.state.currentScreen = this.screens.SCHEDULE){
+      // set the currentScreenComponent to schedule component
+    }
+    else {
+      // set the currentScreenComponent to exams component
+    }
     return (
       <div className="App">
-          {/*just for example. to remove later*/}
-        <Navigation/>
-        <Message headline="ביטול שיעור" content="השיעור באלגברה לינארית בתאריך 10.4.2019 מבוטל"/>
-
+        <Navigation buttonClicked={this._setScreen.bind(this)}/>
+        {currentScreenComponent}
       </div>
     );
   }
--- src/Navigation.js
+++ src/Navigation.js
@@ -2,16 +2,22 @@ import React, { Component } from 'react';
 import './Navigation.css';
 export default class navigation extends Component {
 
-
+  buttonClicked = null;
+  _handleClick(val){
+    this.props.buttonClicked(val);
+  }
   render() {
     const logo = require('./logo200.png');
     return (
       <div className="navigation">
         <div className="nav">
-          <button>מידע כללי</button>
-          <button>מסלול לימודים</button>
-          <button>מערכת שעות</button>
-          <button>לוח מבחנים</button>
+          <div className="nav-btn">
+            <button onClick={this._handleClick(this.value)} value='general'>מידע כללי</button>
+            <button onClick={this._handleClick(this.value)} value='studyPlan'>מסלול לימודים</button>
+            <button onClick={this._handleClick(this.value)} value='schedule'>מערכת שעות</button>
+            <button onClick={this._handleClick(this.value)} value='exams'>לוח מבחנים</button>
+          </div>
+
         </div>
         <div className="logo">
           <img src={logo} alt="logo"/>
