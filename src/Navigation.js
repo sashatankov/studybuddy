6956d622f2c46a21e33c2d13caa0018fed956408
Sasha <isasha96@gmail.com>
2019-05-06 07:45:27 +0300

    personal info button bug fixed

--- src/Navigation.js
+++ src/Navigation.js
@@ -20,7 +20,7 @@ export default class navigation extends Component {
         <div className="nav">
           <div className="nav-btn">
             <button onClick={this._handleClick} value='general'>מידע כללי</button>
-            <button onClick={this._handleClick} value='exams'>מידע אישי</button>
+            <button onClick={this._handleClick} value='personal'>מידע אישי</button>
             <button onClick={this._handleClick} value='studyPlan'>מסלול לימודים</button>
             <button onClick={this._handleClick} value='schedule'>מערכת שעות</button>
             <button onClick={this._handleClick} value='exams'>לוח מבחנים</button>
